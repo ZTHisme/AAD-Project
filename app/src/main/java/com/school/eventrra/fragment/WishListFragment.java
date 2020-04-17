7bb935b0090b1466cb6df6445883ca0a59d6bf7d
Zin-Thu-Htet <zth.chung@gmail.com>
2020-04-17 19:35:23 +0630

    - At wish list fragment, show demo list data.

--- app/src/main/java/com/school/eventrra/fragment/WishListFragment.java
+++ app/src/main/java/com/school/eventrra/fragment/WishListFragment.java
@@ -1,23 +1,47 @@
 package com.school.eventrra.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.school.eventrra.R;
+import com.school.eventrra.activity.EventActivity;
+import com.school.eventrra.adapter.EventRvAdapter;
+import com.school.eventrra.callback.OnRvItemClickListener;
+import com.school.eventrra.model.Event;
+import com.school.eventrra.util.DataSet;
 
-public class WishListFragment extends Fragment {
+public class WishListFragment extends Fragment implements OnRvItemClickListener<Event> {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_wishlist, container, false);
-        final TextView textView = root.findViewById(R.id.text_wishlist);
-        textView.setText("This is wishlist fragment");
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        RecyclerView rv = root.findViewById(R.id.rv);
+        EventRvAdapter adapter = new EventRvAdapter(getContext(), this);
+        // TODO: 4/16/2020 get data from api and set to adapter
+        adapter.setDataSet(DataSet.getDummyEvents());
+        rv.setAdapter(adapter);
+        if (getContext() != null) {
+            rv.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        }
         return root;
+    }
+
+    @Override
+    public void onClick(int position, Event event) {
+        DataSet.selectedEvent = event;
+        startActivity(new Intent(getContext(), EventActivity.class));
+    }
+
+    @Override
+    public void onLongClick(int position, Event event) {
+        // nothing to do
     }
 }
