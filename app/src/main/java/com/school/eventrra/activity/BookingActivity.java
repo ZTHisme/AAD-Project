e736d7592801aa19dc2edfd8be2197278bc3d2b5
Zin-Thu-Htet <zth.chung@gmail.com>
2020-04-17 10:42:11 +0630

    - Booking page UI is implemented.

---
+++ app/src/main/java/com/school/eventrra/activity/BookingActivity.java
@@ -0,0 +1,38 @@
+package com.school.eventrra.activity;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.school.eventrra.R;
+
+public class BookingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_keyboard_arrow_left_white_24dp);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void register(View v) {
+
+    }
+}
--- app/src/main/java/com/school/eventrra/activity/RegisterActivity.java
+++ app/src/main/java/com/school/eventrra/activity/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.school.eventrra.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -43,7 +44,7 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    public void register(View v) {
-
+    public void next(View v) {
+        startActivity(new Intent(this, BookingActivity.class));
     }
 }
